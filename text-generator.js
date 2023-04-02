e75530b094fcbf5e4d82487454fdb1e51f7c3e60
Andrey Ryabtsev <ryab@yandex.ru>
2023-04-02 13:28:01 +0300

    added main func

--- text-generator.js
+++ text-generator.js
@@ -8,3 +8,27 @@ export function setupOutputText(element) {
 
   setOutput('start text');
 };
+
+const getI18nText = function ({ stringTokens, variables, translations, locale }) {
+  let i18nText = '';
+  
+  stringTokens.forEach(token => {
+    if (Array.isArray(token)) {
+      i18nText = i18nText.concat(funcs[token[0]](token.slice(1,)));
+
+      return;
+    }
+
+    if (token.startsWith('#')) {
+      const key = token.substring(1);
+
+      i18nText = i18nText.concat(translations[locale][key]);
+
+      return;
+    }
+    
+    i18nText = i18nText.concat(token);
+  });
+ 
+  return i18nText;
+};
